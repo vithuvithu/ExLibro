f4cf5fe6df6227a115da8b9f1e0e1de9d825bc0b
vithuvithu <138983069+vithuvithu@users.noreply.github.com>
2024-12-05 15:09:08 +0000

    index page php file created

---
+++ ExLibro/Front end/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EXLIBRO</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <header class="navbar">
+        <div class="logo-container">
+            <img src="logo.jpg" alt="Logo" class="logo">
+            
+        </div>
+        <h1> <b> EXLIBRO </b> </h1>
+        <nav>
+            <div class="nav-bar">
+                <ul class="nav-items">
+                    <b><li><a href="#"><span style="position: relative; left: 80px;">Home</span></a></li></b>
+                    <b> <li><a href="#"><span style="position: relative; left: 80px;">About</span></a></li></b>
+                    <b><li><a href="#"><span style="position: relative; left: 80px;">Login</span></a></li></b>
+                   <b><li><a href="#"><span style="position: relative; left: 80px;">Contact</span></a></li></b>
+                </ul>
+            </div>
+        </nav>
+    </header>
+    <h1>Welcome to ExLibro</h1>
+    <div class="about">
+        
+        <p>where your next great read is just a swap away! Dive into a world of endless books, exciting exchanges, and a vibrant community of passionate readers. Whether you’re hunting for a hidden gem or sharing a beloved classic, ExLibro makes it easy to connect, trade, and explore. Join us and turn every page into a new adventure! Happy swapping!</p>
+         
+         
+    </div>
+
+     
+</body>
+</html>
