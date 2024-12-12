b84028c1eafa19cbbb3aeca601a2437e66cfbbee
vithuvithu <138983069+vithuvithu@users.noreply.github.com>
2024-12-12 15:41:12 +0000

    connect database

---
+++ ExLibro/ db_connection.php
@@ -0,0 +1,19 @@
+<?php
+//constant variable
+define('SERVERNAME', '127.0.0.1:3306');
+define('USERNAME', 'root');
+define('PASSWORD', 'mariadb'); 
+define('DBNAME', 'ExLibro');
+try {
+//connect with database
+$connect = mysqli_connect(SERVERNAME, USERNAME, PASSWORD, DBNAME);
+if (!$connect) {
+die("connection failed".mysqli_connect_error());
+}
+else {
+echo "Connected successfully";
+}
+}
+catch (Exception $e) {
+die($e->getMessage());
+}
