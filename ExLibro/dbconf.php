c548a71e06131da8d9f95551307818bd8ff5b288
vithuvithu <138983069+vithuvithu@users.noreply.github.com>
2024-12-12 16:58:09 +0000

    database

--- ExLibro/dbconf.php
+++ ExLibro/dbconf.php
@@ -1,7 +1,7 @@
 <?php
 //constant variable
 define('SERVERNAME', '127.0.0.1:3306');
-define('USERNAME', 'mariadb');
+define('USERNAME', 'root');
 define('PASSWORD', 'mariadb'); 
 define('DBNAME', 'ExLibro');
 try {
