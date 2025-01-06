704e2fc099229a1dfc57d3287e3abb0a81cfcc19
vithuvithu <138983069+vithuvithu@users.noreply.github.com>
2025-01-06 17:22:14 +0000

    commit

--- ExLibro/Backend/login_process.php
+++ ExLibro/Backend/login_process.php
@@ -30,7 +30,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             }
 
             // Redirect to the user profile page or dashboard
-            header("Location: Front end/profile.php");
+            header("Location: Frontend/profile.php");
             exit();
         } else {
             // Incorrect password
