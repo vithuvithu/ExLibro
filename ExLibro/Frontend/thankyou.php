8123f44826eebd8f4458bde8d8facbdabda6b699
vithuvithu <138983069+vithuvithu@users.noreply.github.com>
2025-01-15 16:30:34 +0000

    thankyou php

---
+++ ExLibro/Frontend/thankyou.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EXLIBRO</title>
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+    <header class="navbar">
+        <div class="logo-container">
+             
+ <img src="image/logo.png"alt="Logo" class="logo">';
+             
+           
+            
+        </div>
+        <h1> <b> Thank You for Contacting Us </b> </h1>
+        
+        
+    </header>
+    <h1>Welcome to ExLibro</h1>
+    <div class="about">
+        
+        <p>Thanks for getting in touch with us! Your interest in us is worth your time. We'll get back to you as soon as possible.</p>
+         
+         
+    </div>
+
+     
+</body>
+</html>
