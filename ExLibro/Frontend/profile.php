d4fb83af3dfefcd9d070aedfca3830debb3b7416
vithuvithu <138983069+vithuvithu@users.noreply.github.com>
2025-01-17 09:46:24 +0000

    save

--- ExLibro/Frontend/profile.php
+++ ExLibro/Frontend/profile.php
@@ -1,15 +1,15 @@
 <?php
-// Enable error reporting for debugging
+ 
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-// Start the session
+ 
 session_start();
 
-// Include the database configuration
+ 
 include('../Backend/dbconf.php');
 
-// Check if the user is logged in
+ 
 if (!isset($_SESSION['user_id'])) {
     header("Location: login.php");
     exit();
@@ -22,7 +22,7 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     $user = $result->fetch_assoc();
 } else {
-    // Redirect to an error page or handle the error appropriately
+     
     header("Location: error.php?message=User  not found");
  exit();
 }
